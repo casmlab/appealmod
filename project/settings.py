db7a6a2d24897d75dc58af7823841a411275be17
Vitaliy Lyapota <2vitalik@gmail.com>
2024-06-20 04:22:38 +0300

    Use SECRET_KEY on the server from environ

--- project/settings.py
+++ project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!gc#&e=+sd!goq25_0i$__j$i^h((4^vk+d9d*0+i!brhn58rz'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-!gc#&e=+sd!goq25_0i')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
