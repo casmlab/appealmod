9e7f7b59258b7529da3016037ead255f25a7e186
Vitaliy Lyapota <2vitalik@gmail.com>
2024-07-08 04:39:33 +0300

    sign-up: Add url for `/sign-up/` endpoint

--- project/urls.py
+++ project/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from main.views import IndexView
+from main.views import IndexView, SignUpView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', IndexView.as_view(), name='index')
+    path('', IndexView.as_view(), name='index'),
+    path('sign-up/', SignUpView.as_view(), name='sign-up'),
 ]# todo: + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
