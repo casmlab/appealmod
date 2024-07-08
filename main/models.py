c281b809ac4d68bb9f617ccf42d7e998afa5b026
Vitaliy Lyapota <2vitalik@gmail.com>
2024-07-09 01:34:31 +0300

    ban-appeal: Add first version of DB table for appeals

--- main/models.py
+++ main/models.py
@@ -14,3 +14,25 @@ class SignUpData(models.Model):
 
     def __str__(self):
         return self.email
+
+
+class BanAppealData(models.Model):
+    reddit_username = models.CharField(max_length=254)
+    subreddit = models.CharField(max_length=254)
+
+    why_banned = models.TextField()
+    why_appealing = models.TextField()
+    describe_rule = models.TextField()
+    describe_actions = models.TextField()
+    wrong_actions = models.BooleanField()
+    will_not_repeat = models.BooleanField()
+    what_steps = models.TextField()
+    allowed_comments = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'main_ban_appeal_data'
+
+    def __str__(self):
+        return self.reddit_username
