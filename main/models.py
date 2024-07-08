c212f06bafc4cc5a165dfb9a350a0c92eaaf5eda
Vitaliy Lyapota <2vitalik@gmail.com>
2024-07-08 04:36:44 +0300

    sign-up: Create model SignUpData

--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class SignUpData(models.Model):
+    name = models.CharField(max_length=254)
+    email = models.EmailField()
+    reddit_username = models.CharField(max_length=254)
+    subreddit_url = models.CharField(max_length=254)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'main_sign_up_data'
+
+    def __str__(self):
+        return self.email
