675d1376658cd6eb50c71dca93f94c86842e48b0
Vitaliy Lyapota <2vitalik@gmail.com>
2024-07-09 01:36:18 +0300

    ban-appeals: First views for the ban appeals

--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, CreateView
+from django.views.generic import TemplateView, CreateView, FormView
 
-from main.forms import SignUpForm
+from main.forms import SignUpForm, BanAppealMainForm, BanAppealAuthForm
 
 from django.urls import reverse_lazy
 
@@ -21,3 +21,18 @@ class SignUpView(CreateView):
 
 class SignUpSuccessView(TemplateView):
     template_name = "sign-up/sign-up-success.html"
+
+
+class BanAppealAuthFormView(FormView):
+    template_name = "ban-appeal/auth.html"
+    form_class = BanAppealAuthForm
+
+
+class BanAppealMainFormView(CreateView):
+    template_name = "ban-appeal/form.html"
+    form_class = BanAppealMainForm
+    success_url = reverse_lazy('ban-appeal-thanks')
+
+
+class BanAppealThanksView(TemplateView):
+    template_name = "ban-appeal/thanks.html"
