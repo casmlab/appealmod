41c8f46f90185aa9290ee220fae3d246434ce94a
Vitaliy Lyapota <2vitalik@gmail.com>
2024-07-09 01:35:51 +0300

    ban-appeals: A Couple of forms for auth and appeal

--- main/forms.py
+++ main/forms.py
@@ -1,9 +1,25 @@
 from django import forms
 
-from main.models import SignUpData
+from main.models import SignUpData, BanAppealData
 
 
 class SignUpForm(forms.ModelForm):
     class Meta:
         model = SignUpData
         fields = ['name', 'email', 'reddit_username', 'subreddit_url']
+
+
+class BanAppealAuthForm(forms.Form):
+    reddit_username = forms.CharField(max_length=254)
+    subreddit = forms.CharField(max_length=254)
+
+
+class BanAppealMainForm(forms.ModelForm):
+    class Meta:
+        model = BanAppealData
+        fields = [
+            'why_banned', 'why_appealing',
+            'describe_rule', 'describe_actions',
+            'wrong_actions', 'will_not_repeat',
+            'what_steps', 'allowed_comments',
+        ]
