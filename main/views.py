d3c659e6069f113176363c32c53185e707c99176
Vitaliy Lyapota <2vitalik@gmail.com>
2024-07-09 03:15:39 +0300

    ban-appeals: Send to error page if wrong credentials in GET

--- main/views.py
+++ main/views.py
@@ -5,6 +5,8 @@ from main.forms import SignUpForm, BanAppealMainForm, BanAppealAuthForm
 
 from django.urls import reverse_lazy
 
+from main.models import BanAppealData
+
 
 class IndexView(TemplateView):
     template_name = "index.html"
@@ -39,6 +41,15 @@ class BanAppealMainFormView(CreateView):
     template_name = "ban-appeal/form.html"
     form_class = BanAppealMainForm
     success_url = reverse_lazy('ban-appeal-thanks')
+
+    def get(self, request, *args, **kwargs):
+        reddit_username = request.GET.get('reddit_username')
+        subreddit = request.GET.get('subreddit')
+        if not BanAppealData.objects.auth(reddit_username, subreddit):
+            url = reverse_lazy('ban-appeal-error') + \
+                  f'?reddit_username={reddit_username}&subreddit={subreddit}'
+            return redirect(url)
+        return super().get(request, *args, **kwargs)
 
 
 class BanAppealErrorView(TemplateView):
