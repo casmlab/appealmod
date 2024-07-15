ba15b45f72cdb94b176010622f44fd4662a5657b
Vitaliy Lyapota <2vitalik@gmail.com>
2024-07-15 22:50:05 +0300

    admin: Configured two first admin pages

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from main.models import SignUpData, BanAppealData
+
+
+@admin.register(SignUpData)
+class SignUpDataAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'reddit_username', 'subreddit_url',
+                    'created_at')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', )
+    list_per_page = 20
+
+
+@admin.register(BanAppealData)
+class BanAppealDataAdmin(admin.ModelAdmin):
+    list_display = ('id', 'reddit_username', 'subreddit', 'created_at')
+    list_display_links = ('id', 'reddit_username')
+    search_fields = ('reddit_username', )
+    list_per_page = 20
