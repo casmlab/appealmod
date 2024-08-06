b92711ca6fc1954c9953d9422040d5b8e6c2e42e
Vitaliy Lyapota <2vitalik@gmail.com>
2024-08-06 03:19:55 +0300

    logger: Create func `get_logger`

--- core/scripts/redditLogging.py
+++ core/scripts/redditLogging.py
@@ -37,27 +37,33 @@ class MongoDBLogger(StreamHandler):
         })
 
 
-logger = logging.getLogger('main_logger')
-logger.setLevel(logging.INFO)
+def get_logger():
+    logger = logging.getLogger('main_logger')
+    logger.setLevel(logging.INFO)
 
-formatter = logging.Formatter('%(asctime)s,%(msecs)03d %(levelname)-8s '
-                              '[%(filename)s:%(lineno)d] %(message)s',
-                              datefmt='%Y-%m-%d:%H:%M:%S')
+    formatter = logging.Formatter('%(asctime)s,%(msecs)03d %(levelname)-8s '
+                                  '[%(filename)s:%(lineno)d] %(message)s',
+                                  datefmt='%Y-%m-%d:%H:%M:%S')
 
-file_handler = logging.FileHandler("redditLogging.log")
-file_handler.setFormatter(formatter)
-file_handler.setLevel(logging.INFO)
-logger.addHandler(file_handler)
+    file_handler = logging.FileHandler("redditLogging.log")
+    file_handler.setFormatter(formatter)
+    file_handler.setLevel(logging.INFO)
+    logger.addHandler(file_handler)
 
-mongo_logger = MongoDBLogger()
-mongo_logger.setFormatter(formatter)
-mongo_logger.setLevel(logging.INFO)
-logger.addHandler(mongo_logger)
+    mongo_logger = MongoDBLogger()
+    mongo_logger.setFormatter(formatter)
+    mongo_logger.setLevel(logging.INFO)
+    logger.addHandler(mongo_logger)
 
-console_handler = logging.StreamHandler(sys.stdout)
-console_handler.setFormatter(formatter)
-console_handler.setLevel(logging.INFO)
-logger.addHandler(console_handler)
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(formatter)
+    console_handler.setLevel(logging.INFO)
+    logger.addHandler(console_handler)
+
+    return logger
+
+
+logger = get_logger()
 
 
 class SafeJSONEncoder(json.JSONEncoder):
