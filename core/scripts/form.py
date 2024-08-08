5de7099d5b27437e77a0c0822545db7fc6355da4
Vitaliy Lyapota <2vitalik@gmail.com>
2024-08-09 01:30:25 +0300

    core/form: Function to interact with form data (add and get)

---
+++ core/scripts/form.py
@@ -0,0 +1,10 @@
+import core.scripts.bin  # to enable django support
+from main.models import BanAppealData
+
+
+def add_form_entry(reddit_username, subreddit):
+    BanAppealData.objects.create(reddit_username, subreddit)
+
+
+def get_form_response(reddit_username, subreddit):
+    return BanAppealData.objects.auth(reddit_username, subreddit)
