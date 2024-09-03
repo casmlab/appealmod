c25ce33ac0d7b575657ff440eb5bffab7819baf7
Vitaliy Lyapota <2vitalik@gmail.com>
2024-09-03 22:25:54 +0300

    minor: Several renames in `db`

--- core/scripts/db/db.py
+++ core/scripts/db/db.py
@@ -5,9 +5,10 @@ from core.config import Config as config
 
 class Database:
     def __init__(self):
-        cluster = MongoClient(config.DB_CONNECTION_STRING)
-        self.database = cluster['main-cluster']
-        self.responses = self.database['bot-responses']
+        client = MongoClient(config.DB_CONNECTION_STRING)
+        cluster = client['main-cluster']
+
+        self.responses = cluster['bot-responses']
 
     def get_responses(self, subreddit):
         cursor = self.responses.find({'subreddit': subreddit})
