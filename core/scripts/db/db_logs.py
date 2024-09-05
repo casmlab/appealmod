d186cf6702c402be1a51749e0b6fb6f56d3f42f1
Vitaliy Lyapota <2vitalik@gmail.com>
2024-09-05 19:33:08 +0300

    bug-fix: Fix method name

--- core/scripts/db/db_logs.py
+++ core/scripts/db/db_logs.py
@@ -5,7 +5,7 @@ class DbLogs:
     def __init__(self, collection):
         self.collection = collection
 
-    def log(self, message, subreddit, conv_id):
+    def add(self, message, subreddit, conv_id):
         self.collection.insert_one({
             "message": message,
             "timestamp": datetime.now(),
