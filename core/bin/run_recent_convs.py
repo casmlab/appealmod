6c5c51fb23d1ea43a66bb7d061500a55b87ef573
Vitaliy Lyapota <2vitalik@gmail.com>
2024-09-05 19:33:30 +0300

    bin: Create to starting files

---
+++ core/bin/run_recent_convs.py
@@ -0,0 +1,5 @@
+import core.bin.django_bin  # to enable django support
+from core.scripts.runners.recent_convs import run_recent_convs
+
+if __name__ == "__main__":
+    run_recent_convs()
---
+++ core/bin/run_started_convs.py
@@ -0,0 +1,5 @@
+import core.bin.django_bin  # to enable django support
+from core.scripts.runners.started_convs import run_started_convs
+
+if __name__ == "__main__":
+    run_started_convs()
