52fcd1a0b54dbad722aaabafa298f3615ec3df78
Vitaliy Lyapota <2vitalik@gmail.com>
2024-09-22 20:21:55 +0300

    conf: Add common `conf` (both for `web` and `bot`)

---
+++ conf/conf.py
@@ -0,0 +1,11 @@
+slack_disabled = False
+
+slack_hook_status = None  # should be set in `local_conf.py`
+slack_hook_alerts = None  # should be set in `local_conf.py`
+slack_hook_errors = None  # should be set in `local_conf.py`
+slack_hook_logging = None
+
+try:
+    from .local_conf import *
+except ImportError:
+    pass
