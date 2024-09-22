760b338498e0823790d18753089d203fe9be7c91
Vitaliy Lyapota <2vitalik@gmail.com>
2024-09-22 17:51:49 +0300

    rename: `core` -> `bot` (in the root)

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-exec python3 core/bin/run_recent_convs.py &
-exec python3 core/bin/run_started_convs.py
+exec python3 bot/bin/run_recent_convs.py &
+exec python3 bot/bin/run_started_convs.py
