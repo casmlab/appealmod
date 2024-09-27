ffbbe9c340f6347f5baebf268dedc71162a61061
Vitaliy Lyapota <2vitalik@gmail.com>
2024-09-27 03:30:08 +0300

    slack: Add additional functions for links

--- utils/slack/styling.py
+++ utils/slack/styling.py
@@ -1,3 +1,14 @@
+from bot.conf import conf
+
+
+def rlink(subreddit):
+    subreddit_link = f'https://reddit.com/r/{subreddit}'
+    return f'<{subreddit_link}|{subreddit}>'
+
+
+def clink(conv_id):
+    conv_link = f'https://mod.reddit.com/mail/all/{conv_id}'
+    return f'<{conv_link}|{conv_id}>'
 
 
 def sl(job, subreddit, conv_id, text):
@@ -7,8 +18,7 @@ def sl(job, subreddit, conv_id, text):
         'D': ':speech_balloon:',
     }[job]
 
-    subreddit_link = f'https://reddit.com/r/{subreddit}'
-    conv_link = f'https://mod.reddit.com/mail/all/{conv_id}'
+    return f'{job_icon} [{rlink(subreddit)} :: <{clink(conv_id)}]: {text}'
 
     return f'{job_icon} [<{subreddit_link}|{subreddit}> :: ' \
            f'<{conv_link}|{conv_id}>]: {text}'
