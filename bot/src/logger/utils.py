09e02a4aa142f7f8eb61463416273fbe2d438066
Vitaliy Lyapota <2vitalik@gmail.com>
2024-10-02 21:19:19 +0300

    Remove redundant things

--- bot/src/logger/utils.py
+++
@@ -1,6 +0,0 @@
-from bot.src.logger.logger import logger
-
-
-def log(message, conv_id=None, subreddit=None):
-    logger.info(message, extra={'conv_id': conv_id,
-                                'subreddit': subreddit})
